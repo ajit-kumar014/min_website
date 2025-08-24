05e2d799c91355038a70a1aec696bac805818e50
Ajit kumar <67961633+ajit-kumar014@users.noreply.github.com>
2025-08-24 22:03:44 +0530

    Update data.ts

--- app/data.ts
+++ app/data.ts
@@ -40,7 +40,7 @@ export const PROJECTS: Project[] = [
   {
     name: 'HomeServer',
     description: 'Self-Hosted Home Server setup using my old laptop',
-    link: 'https://motion-primitives.com/',
+    link: 'https://youtu.be/yul4gq_LrOI',
     video:
       'https://youtu.be/yul4gq_LrOI',
     id: 'project2',
